f3b53fd98788f2f30e0a740884079e8cc04af114
shahnmah <shahn.mah@gmail.com>
2022-04-11 23:53:35 +0600

    provider added

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -11,5 +11,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import googleIcon from '../../images/google.png'
+import facebookIcon from '../../images/facebook.png'
+import './Login.css'
+
 const Login = () => {
     return (
         <div className='w-25 mx-auto my-5'>
@@ -17,6 +21,14 @@ const Login = () => {
                 <Button className='w-100' variant="primary" type="submit">
                     Submit
                 </Button>
+                <div className='my-4 d-flex justify-content-between'>
+                <Button className='provider-icon' variant="transparent" type="submit">
+                    <img src={googleIcon} alt="" />
+                </Button>
+                <Button className='provider-icon' variant="transparent" type="submit">
+                    <img src={facebookIcon} alt="" />
+                </Button>
+                </div>
             </Form>
         </div>
     );
